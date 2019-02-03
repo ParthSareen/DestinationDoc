19127d2ea99234ed7a384d749c224a26c02d8723
Parth <psareen@uwaterloo.ca>
2019-02-03 06:43:35 -0500

    updated coordinates

--- testApp.py
+++ testApp.py
@@ -9,24 +9,32 @@ app = Flask(__name__) #create the Flask app
 
 @app.route('/query-example')
 def query_example():
-    url = 'https://dev.virtualearth.net/REST/v1/LocalSearch/?query=%s&userLocation=%s,%s&key=AvZXOOO79Ko1HoL-HN1VuNQU0qtL842kjYVWuh3dL1uBLawZzHb9yydtD8rqhduA' % ('physiotherapy', '47.602038', '-122.333964')
-    print(url)
-    re = requests.get(url)
-    returning = json.loads(re.text)
-    #return returning['resourceSets']['0']['estimatedTotal']
-    print(type(returning['resourceSets'][0]['resources']))
-    print(returning['resourceSets'][0]['resources'][0]['name'])
-    for i in len(returning['resourceSets'][0]['resources']):
-    	print(returning['resourceSets'][0]['resources'][i]['name'])
-    return str(returning['resourceSets'][0]['estimatedTotal'])
+	categ=request.args["categ"]
+	lat=request.args["lat"]
+	longs=request.args["longs"]
+	url = 'https://dev.virtualearth.net/REST/v1/LocalSearch/?query=%s&userLocation=%s,%s&key=AvZXOOO79Ko1HoL-HN1VuNQU0qtL842kjYVWuh3dL1uBLawZzHb9yydtD8rqhduA' % (categ, lat, longs)
+	#print(url)
+	re = requests.get(url)
+	returning = json.loads(re.text)
+	#return returning['resourceSets']['0']['estimatedTotal']
+	#print(type(returning['resourceSets'][0]['resources']))
+	#print(returning['resourceSets'][0]['resources'][0]['point']['coordinates'])
+	Dict = {}
+	for i in range(len(returning['resourceSets'][0]['resources'])-1):
+		Result_name = returning['resourceSets'][0]['resources'][i]['name']
+		Result_coordinates=returning['resourceSets'][0]['resources'][i]['point']['coordinates']
 
-@app.route('/form-example')
-def formexample():
-    return 'Todo...'
+		print(Result_name)
+		Dict[str(i)] = [Result_name,Result_coordinates]
+		return json.dumps(Dict)
+
+		@app.route('/form-example')
+		def formexample():
+			return 'Todo...'
 
 @app.route('/json-example')
 def jsonexample():
-    return 'Todo...'
+   return 'Todo...'
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) #run app in debug mode on port 5000
+   app.run(debug=True, port=5000) #run app in debug mode on port 5000
